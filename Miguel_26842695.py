0a906e8db571b2fafa2d0b8777172a1a29a87c21
Edgard Decena <edecena@gmail.com>
2019-10-31 19:22:53 -0400

    Primer commit.

---
+++ Miguel_26842695.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Proyecto en Python sobre el Algoritmo de Bisección.
+
+Cada participante debe completar su módulo y luego solicitar el Pull-Request.
+"""
+
+import math
+
+
+def biseccion(f, a, b, ER, N):
+    """
+    Implementa el Algoritmo de Biseccion y retorna la aproximación de la raiz.
+
+    Parámetros:
+    f: función de variable real f(x).
+    a: límite inferior del intervalo.
+    b: límite superior del intervalo.
+    ER: cota mínima del error relativo.
+    N: número máximo de iteraciones.
+    """
+
+    print("Iteración:", i, "Punto Medio:", pm_actual, "Error:", err)
+
+    return pm_actual
+
+
+if __name__ == "__main__":
+    # Pruebe aquí su función.
+    pass
