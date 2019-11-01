4bc9c321aba8c2460f9bbdb3e2935e5781e1b9aa
Enrique Salazar <xeast98@gmail.com>
2019-11-01 09:41:42 -0430

    Update Enrique_27000054.py

--- Enrique_27000054.py
+++ Enrique_27000054.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python3
-"""
-Proyecto en Python sobre el Algoritmo de Bisección.
+from math import *
 
-Cada participante debe completar su módulo y luego solicitar el Pull-Request.
-"""
+def biseccion (f,a,b,es,ni):
 
-import math
-
-
-def biseccion(f, a, b, ER, N):
-    """
-    Implementa el Algoritmo de Biseccion y retorna la aproximación de la raiz.
-
-    Parámetros:
-    f: función de variable real f(x).
-    a: límite inferior del intervalo.
-    b: límite superior del intervalo.
-    ER: cota mínima del error relativo.
-    N: número máximo de iteraciones.
-    """
-
-    print("Iteración:", i, "Punto Medio:", pm_actual, "Error:", err)
-
-    return pm_actual
-
+    if (f(a)*f(b))>0 :
+        print ("Intervalo no valido")
+        return
+    error= 100; x_anterior = 0; x_actual=0; n=1;
+    while (n<ni) and (error>es):
+        m =(a+b)/2.0
+        x_actual= m
+        error= abs(x_actual-x_anterior)/x_actual
+        if (f(a)*f(m)<0):
+            b=m
+        else:
+            a=m
+        x_anterior=x_actual
+        print ("Iteracion: ",n, " Punto medio: ", x_actual, " Error: ", error)
+        n= n + 1
+    print (x_actual)
+    return (x_actual)
+    
+    
 
 if __name__ == "__main__":
     # Pruebe aquí su función.
+    f=lambda x: x - cos(x);
+    biseccion(f, 0, 1, 0.02, 10)
     pass
+
